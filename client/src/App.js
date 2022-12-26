5a588335dc3c3680f2ab9846eb64f85776e63c6e
m.indra.dev <sirtwewew@gmail.com>
2022-12-26 22:08:18 +0700

    added blog pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,25 @@
-export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/single" element={<Single />} />
+          <Route path="/write" element={<Write />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+export default App;
