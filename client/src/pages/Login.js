3df16d9fed18c528236778649b47af39696dbb1f
m.indra.dev <sirtwewew@gmail.com>
2022-12-28 22:55:58 +0700

    update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -24,6 +24,7 @@ const Login = () => {
                   type="text"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Email address"
+                  required
                 />
               </div>
               {/* Password input */}
@@ -32,6 +33,7 @@ const Login = () => {
                   type="password"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Password"
+                  required
                 />
               </div>
               <div className="flex justify-between items-center mb-6">
